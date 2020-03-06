7e54499aa8545c2aa2d87fd7bf2d6a9a630d9bc1
weichie <bobmasaki@hotmail.com>
2020-03-06 08:38:18 +0100

    Login route

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -77,6 +77,33 @@ const isEmail = (email) => {
   else return false;
 }
 
+// -- Login route ----------
+app.post('/login', (req, res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password,
+  };
+
+  let errors = {};
+
+  if (isEmpty(user.email)) errors.email = 'Must not be empty';
+  if (isEmpty(user.password)) errors.password = 'Must not be empty';
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+  firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+    .then(data => {
+      return data.user.getIdToken();
+    })
+    .then(token => {
+      return res.json({ token });
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+});
+
 // -- Signup route ----------
 app.post('/signup', (req, res) => {
   const newUser = {
